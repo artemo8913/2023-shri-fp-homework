340999d7dbcd8b43a7323005e8365c281379e888
Spiryaev <kaline-son@ya.ru>
2023-07-24 02:42:12 +0700

    fix:processSeq

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,88 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import {
+  __,
+  allPass,
+  andThen,
+  assoc,
+  compose,
+  length,
+  concat,
+  gt,
+  ifElse,
+  lt,
+  prop,
+  test,
+  mathMod,
+  otherwise,
+} from "ramda";
+import Api from "../tools/api";
+const consoleLog = (a) => {
+  console.log(a);
+  return a;
+};
+//
+const numRound = compose(Math.round, Number);
+const getLength = prop("length");
+const getNumLength = compose(length, String);
+const thenGetLength = andThen(getNumLength);
+const square = (num) => num * num;
+const thenSquare = andThen(square);
+const modForThreeToString = compose(String, mathMod(__, 3), Number);
+const andThenModForThreeToString = andThen(modForThreeToString);
+// API
+const api = new Api();
+const NUMBER_API_URL = "https://api.tech/numbers/base";
+const ANIMALS_API_URL = "https://animals.tech/";
+const getApiResult = compose(String, prop("result"));
+const assocNumberToBinary = assoc("number", __, { from: 10, to: 2 });
+const apiGetNumberBinaryBase = compose(api.get(NUMBER_API_URL), assocNumberToBinary);
+const thenGetApiResult = andThen(getApiResult);
+const thenConcatToAnimalsUrl = andThen(concat(ANIMALS_API_URL));
+const thenCallApiWithEmptyParams = andThen(api.get(__, {}));
+// Валидация
+const lessThenTen = lt(__, 10);
+const greaterThenTwo = gt(__, 2);
+const greaterThenZero = gt(__, 0);
+const isStrNumLessThenTen = compose(lessThenTen, getLength);
+const isStrNumMoreThenTwo = compose(greaterThenTwo, getLength);
+const numIsPositive = compose(greaterThenZero, Number);
+const isOnlyNumbersInStr = test(/^[0-9]+\.?[0-9]+$/);
+const validate = allPass([isStrNumLessThenTen, isStrNumMoreThenTwo, numIsPositive, isOnlyNumbersInStr]);
 
- const api = new Api();
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const logError = () => handleError("ValidationError");
+  const log = (a) => {
+    writeLog(a);
+    return a;
+  };
+  const thenLog = andThen(log);
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+  const thenHandleSuccess = andThen(handleSuccess);
+  const otherwiseHandleError = otherwise(handleError);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
-
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
-
-     wait(2500).then(() => {
-         writeLog('SecondLog')
-
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
-
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  const composition = compose(
+    otherwiseHandleError,
+    thenHandleSuccess,
+    thenGetApiResult,
+    thenCallApiWithEmptyParams,
+    thenConcatToAnimalsUrl,
+    thenLog,
+    andThenModForThreeToString,
+    thenLog,
+    thenSquare,
+    thenLog,
+    thenGetLength,
+    thenLog,
+    thenGetApiResult,
+    apiGetNumberBinaryBase,
+    log,
+    numRound,
+    log
+  );
+  const runValidated = ifElse(validate, composition, logError);
+  runValidated(value);
+};
 
 export default processSequence;
